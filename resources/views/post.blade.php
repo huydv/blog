ccc80882a6950781c967a0ba20cd85957ead8272
Huy Duong Van <huydv@hblab.vn>
2021-10-12 00:59:25 +0700

    Initial project blog

---
+++ resources/views/post.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <x-slot name="content">
+        <article>
+            <h1>{{ $post->title }}</h1>
+            <div>{!! $post->body !!}<div>
+        </article>
+
+        <a href="/">Go back</a>
+    </x-slot>
+</x-layout>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+    <x-slot name="content">
+       @foreach ($posts as $post)
+            <article>
+                <h1>
+                    <a href="/posts/{{ $post->slug }}">
+                        {{ $post->title }}
+                    </a>
+                </h1>
+                <div>
+                    {{ $post->excerpt }}
+                </div>
+            </article>
+       @endforeach
+    </x-slot>
+</x-layout>
+
+
